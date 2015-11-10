b49274e2bb96c733b1540640be48e6ad15d69b1e
Pedro Alvarado <pedro.alvarado@globant.com>
2015-11-10 16:56:28 -0500

    Mocking data with mirage http://www.ember-cli-mirage.com/docs/v0.0.27/

---
+++ app/mirage/fixtures/todos.js
@@ -0,0 +1,5 @@
+export default [
+  {id: 1, title: 'Zelda', complete: false},
+  {id: 2, title: 'Link', complete: false},
+  {id: 3, title: 'Epona', complete: false},
+];
--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,16 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let todos =  [
-      {
-        title: 'Learn Ember',
-        complete: false
-      },
-      {
-        title: 'Solve the planet',
-        complete: false
-      }
-    ];
-    return todos;
+    this.store.findAll('todo');
   }
-})
+});
